9827a0604916bfee5a01c75df507db2e9447c413
maybesushantt <215401027@gkv.ac.in>
2024-08-10 13:38:38 +0530

    Update check-visibility.js

--- api/check-visibility.js
+++ api/check-visibility.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  const isVisible = false; // Change this to false to make the site invisible
+  const isVisible = true; // Change this to false to make the site invisible
   res.status(200).json({ visible: isVisible });
 }
